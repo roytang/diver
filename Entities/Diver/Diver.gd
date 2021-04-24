483ad54f28fdcf44d90ddf6cddf48519c099c02e
Roy Tang <4228+roytang@users.noreply.github.com>
2021-04-25 01:17:33 +0800

    Added depth score to game over screen, and some quotes. Made seekers slightly bigger

--- Entities/Diver/Diver.gd
+++ Entities/Diver/Diver.gd
@@ -107,6 +107,7 @@ func _on_AnimationPlayer_animation_finished(anim_name):
 			# TODO Death, game over, whatever
 			get_tree().queue_delete(self)
 			get_tree().root.get_node("Root/AnimationPlayer").play("GameOver")
+			get_tree().root.get_node("Root/CanvasLayer/GameOver").set_max_depth(max_depth)
 			get_tree().root.get_node("Root/CanvasLayer/GameOver").set_process_input(true)
 			# get_tree().paused = true
 
