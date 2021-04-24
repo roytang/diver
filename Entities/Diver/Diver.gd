1f5897de5e40994ec7b3f91813fdc0e2163b1e47
Roy Tang <4228+roytang@users.noreply.github.com>
2021-04-24 14:11:47 +0800

    Added bubblers and air bubbles

--- Entities/Diver/Diver.gd
+++ Entities/Diver/Diver.gd
@@ -1,5 +1,6 @@
 extends KinematicBody2D
 
+signal breath
 
 var gravity = 80
 var walkspeed = 60
@@ -8,7 +9,13 @@ var bounce_cooldown = 1000
 var next_bounce = 0
 var bounce_to = 0
 var bounce_time = 800
+var next_breath = 0
+var breath_cooldown = 500
 var velocity = Vector2()
+
+var oxygen = 100
+
+var tiny_bubble_scene = preload("res://Entities/TinyBubble/TinyBubble.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -37,15 +44,33 @@ func _physics_process(delta):
 		velocity.y = gravity
 	else:
 		velocity.y = -bounce
+		
+	if now >= next_breath:
+		next_breath = now + breath_cooldown
+		oxygen = oxygen - 1
+		
+		# emit a bubble for flavor
+		var bubble = tiny_bubble_scene.instance()
+		bubble.position = position
+		get_tree().root.get_node("Root").add_child(bubble)
+		
+		emit_signal("breath", self)
+		
+		
 	get_input()
 	var motion = (velocity*delta)
 	var collision = move_and_collide(motion)	
 	if collision:
+		print(collision.collider.name)
 		if collision.collider.name == "Spikes":
 			# YOU'LL DIE!
-			$CollisionShape2D.disabled
 			set_process(false)
 			$AnimationPlayer.play("Death")
+			# TODO Death, game over, whatever
+		if collision.collider.name == "AirBubble":
+			oxygen = clamp(oxygen + 25, 0, 100)
+			emit_signal("breath")
+			collision.collider.queue_free()
 
 
 func _on_AnimationPlayer_animation_finished(anim_name):
